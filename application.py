d3ae383d004bd79f39552fdbdcfeaa7dbaa01d49
Noobmaster23 <51075975+Noobmaster23@users.noreply.github.com>
2020-11-12 20:24:11 +0100

    testing

--- application.py
+++ application.py
@@ -73,6 +73,17 @@ def make_thumbnail_image(gallery_path, img_name):
 root = tkinter.Tk()
 root.title("Heebphotography.ch")
 root.wm_iconbitmap("favicon.ico")
+# frames
+db_upload = tkinter.Frame(root)
+#est
+test_label = tkinter.Label(db_upload, text="test")
+def testlol():
+    print("lol")
+    db_upload.grid_forget()
+test_button = tkinter.Button(db_upload, command=testlol, text="test lol")
+test_label.pack()
+test_button.pack()
+db_upload.grid(row=15, column=0, columnspan=2)
 # connect to database
 password = simpledialog.askstring(
     title="Database Password", prompt="Password:")
